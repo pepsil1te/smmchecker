4e2403e01dd8821506a11aa49da264f08d9e4eb0
Sergey <quintinfine@gmail.com>
2024-02-26 16:36:15 +0300

    2

--- main.py
+++ main.py
@@ -8,14 +8,18 @@ TOKEN = '7125958977:AAHMRgilpAgJ1kINLXk4A7OUcWfPkB_ZGZ4'
 # Замените список 'TARGET_PHRASES' на вариации словосочетаний, которые вы хотите проверять в сообщениях
 TARGET_PHRASES = ['@vozmishev', ':@vozmishev', ': @vozmishev', '@gifesttt', ':@gifesttt', ': @gifesttt']
 
+admin_id = 753113351
+moder1_id = 925051239
+moder2_id = 789022369
+
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(func=lambda message: True)
 def check_message(message):
     chat_id = message.chat.id
     message_text = message.text.lower()
-
-    if not any(phrase in message_text for phrase in TARGET_PHRASES):
+    user_id = message.from_user.id
+    if not any(phrase in message_text for phrase in TARGET_PHRASES) and user_id not in [admin_id, moder1_id, moder2_id]:
         # Удаляем сообщение
         bot.delete_message(chat_id, message.message_id)
         user_id = message.from_user.id
